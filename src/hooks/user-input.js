6b5b5e8237968f6cd7387810c989cd9296019583
ziaurrahaman <ziaurrahaman93i@gmail.com>
2021-12-26 19:32:32 +0600

    some code added

--- src/hooks/user-input.js
+++ src/hooks/user-input.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 const UserInput = (props) => {
-  const [enteredName, setEnteredName] = useState("");
-  const [isEnterNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const valueIsValid = enteredName.trim() !== "";
+  const hasError = !enteredNameIsValid && isEnterNameTouched;
 };
 export default UserInput;
